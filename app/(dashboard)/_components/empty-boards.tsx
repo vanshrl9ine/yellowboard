0ffff77c387f8fbc154a7cabb547c2a8933784e2
vanshrl9ine <guptavansh67@gmail.com>
2024-08-15 22:18:38 +0530

    empty states

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,14 +1,33 @@
+
+'use client'
 import React from 'react';
-import { ThumbsDown } from 'lucide-react';
+import { AppWindow, ThumbsDown } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
-
+import { useMutation } from 'convex/react';
+import { api } from '@/convex/_generated/api';
+import { useOrganization } from '@clerk/nextjs';
 const EmptyBoards = () => {
+  const create=useMutation(api.board.create);
+  const {organization}=useOrganization();
+
+  const onClick=()=>{
+    if(!organization)return;
+    create({
+      orgId:organization.id,
+      title:"Untitled"
+    })
+  }
   return (
     <div className="h-full flex flex-col items-center justify-center space-y-3">
-      <ThumbsDown className="h-[100px] w-[100px]" />
+      <AppWindow className="h-[100px] w-[100px]" />
       <div>
-        <h4>No boards to show at all!</h4>
+        <h4>Create your first board</h4>
+      </div>
+      <div className="mt-6 ">
+        <Button onClick={onClick}>
+          Create Board
+        </Button>
       </div>
     </div>
   );
