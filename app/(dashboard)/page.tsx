ea09d8d55c63a3df4ec4d55680ae0227f7d7c78e
vanshrl9ine <guptavansh67@gmail.com>
2024-08-15 16:51:57 +0530

    dashboard props

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -5,10 +5,18 @@ import { useOrganization, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import EmptyOrg from "./_components/empty-org";
 
-export default function Home() {
+
+interface DashboardProps{
+  searchParams:{
+    search?:string;
+    favorites?:string;
+  };
+};
+export default function DashboardPage({searchParams}:DashboardProps) {
   const {organization}=useOrganization();
   return (
     <div className="flex-1 h-[calc(100%-80px)] p-6">
+      {JSON.stringify(searchParams)}
       {!organization?<EmptyOrg/>:<p>Board list</p>}
       
     </div>
