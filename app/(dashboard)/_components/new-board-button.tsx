da32315db36043f29c79f274efae4aafffabc62f
vanshrl9ine <guptavansh67@gmail.com>
2024-08-16 00:26:06 +0530

    cards

--- app/(dashboard)/_components/new-board-button.tsx
+++ app/(dashboard)/_components/new-board-button.tsx
@@ -1,20 +1,34 @@
 'use client';
+import { api } from '@/convex/_generated/api';
+import { UseApiMutation } from '@/hooks/use-api-mutation';
 import { cn } from '@/lib/utils';
 import { Plus } from 'lucide-react';
 import React from 'react'
+import { toast } from 'sonner';
 interface NewBoardButtonProps{
     orgId:string
     disabled?:boolean
 }
 export const NewBoardButton = ({orgId,disabled}:NewBoardButtonProps) => {
 
-    
+    const {mutate,pending}=UseApiMutation(api.board.create);
+    const onClick=()=>{
+        mutate({
+            orgId,
+            title:"Untitled"
+        })
+        .then((id)=>{
+            toast.success("Board Created")
+            //TODO:Redirect to /board/id
+        })
+        .catch(()=>{toast.error("Failed to create board")})
+    }
   return (
     
     <button
-    disabled={disabled}
-    onClick={()=>{}}
-    className={cn("col-span-1 aspect-[100/127] bg-blue-600 rounded-lg hover:bg-blue-800 flex flex-col items-center justify-center py-6",disabled&& "opacity-75")}
+    disabled={pending || disabled}
+    onClick={onClick}
+    className={cn("col-span-1 aspect-[100/127] bg-blue-600 rounded-lg hover:bg-blue-800 flex flex-col items-center justify-center py-6",(pending || disabled)&& "opacity-75")}
     >
     <div/>
     <Plus
