3d369bd5141eb0d04c4c3a4aafccb54c0352f812
vanshrl9ine <guptavansh67@gmail.com>
2024-08-18 03:41:07 +0530

    Final

--- app/board/[boardId]/_components/cursors-presence.tsx
+++ app/board/[boardId]/_components/cursors-presence.tsx
@@ -1,7 +1,9 @@
 'use client';
 import { memo } from "react";
-import { useOthersConnectionIds } from "@liveblocks/react/suspense";
+import { shallow, useOthersConnectionIds, useOthersMapped } from "@liveblocks/react/suspense";
 import { Cursor } from "./cursor";
+import { colorToCss } from "@/lib/utils";
+import { Path } from "./path";
 
 const Cursors = () => {
     const ids = useOthersConnectionIds();
@@ -14,10 +16,38 @@ const Cursors = () => {
       </>
     );
   };
+  const Draft = () => {
+    const others = useOthersMapped(
+      (other) => ({
+        pencilDraft: other.presence.pencilDraft,
+        pencilColor: other.presence.pencilColor,
+      }),
+      shallow
+    );
+  
+    return (
+      <>
+        {others.map(([key, other]) => {
+          if (other.pencilDraft) {
+            return (
+              <Path
+                key={key}
+                x={0}
+                y={0}
+                points={other.pencilDraft}
+                fill={other.pencilColor ? colorToCss(other.pencilColor) : "#000"}
+              />
+            );
+          }
+          return null;
+        })}
+      </>
+    );
+  };
   export const CursorsPresence = memo(() => {
     return (
       <>
-      
+        <Draft/>
         <Cursors />
       </>
     );
